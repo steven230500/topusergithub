f2149c2c13ab77a553adcde5165b5d84ba289944
steven230500 <steven.patino@asap507.com>
2022-11-12 12:03:32 -0500

    person styles

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,28 +1,34 @@
 import React from 'react';
-import {StyleSheet, View, Text, ActivityIndicator} from 'react-native';
-import { useEffect, useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  Text,
+  ActivityIndicator,
+  TouchableHighlight,
+  Image,
+} from 'react-native';
+import {useEffect, useState} from 'react';
 
 const Person = ({route, navigation}) => {
-    let [isLoading, setIsLoading] = useState(true);
+  let [isLoading, setIsLoading] = useState(true);
   let [error, setError] = useState();
-  let [response, setResponse] = useState();
+  let [response, setResponse] = useState({});
 
   const {user} = route.params;
 
-
   useEffect(() => {
-    fetch("https://api.github.com/users/"+user)
+    fetch('https://api.github.com/users/' + user)
       .then(res => res.json())
       .then(
-        (result) => {
+        result => {
           setIsLoading(false);
           setResponse(result);
         },
-        (error) => {
+        error => {
           setIsLoading(false);
           setError(error);
-        }
-      )
+        },
+      );
   }, []);
 
   const getContent = () => {
@@ -31,27 +37,49 @@ const Person = ({route, navigation}) => {
     }
 
     if (error) {
-      return <Text>{error}</Text>
+      return <Text>{error}</Text>;
     }
-    console.log(response["avatar_url"])
-    
-    return <Text>Blog {response["blog"]}</Text>;
+
+    return (
+      <View style={{alignItems: 'center'}}>
+        <View style={[styles.profileImgContainer, {borderWidth: 1}]}>
+          <Image
+            source={{uri: response['avatar_url']}}
+            style={styles.profileImg}
+          />
+        </View>
+        <Text style={styles.textStyle}>Name: {response['name']}</Text>
+        <Text style={styles.textStyle}>Blog: {response['blog']}</Text>
+        <Text style={styles.textStyle}>Company: {response['company']}</Text>
+      </View>
+    );
   };
 
-  return (
-    <View style={styles.container}>
-      {getContent()}
-    </View>
-  );
+  return <View style={styles.container}>{getContent()}</View>;
 };
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+  },
+  profileImgContainer: {
+    overflow: 'hidden',
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+  },
+  profileImg: {
+    height: 120,
+    width: 120,
+    borderRadius: 60,
+  },
+  textStyle: {
+    marginTop: 20,
+    textAlign: 'center',
+  },
+});
 
 export default Person;
